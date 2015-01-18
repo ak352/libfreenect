e41d190853f98b981848a6be6c9345c5e6d65bc8
Abhimanyu KRISHNA <abhimanyu.krishna@admins-macbook-pro-5.fritz.box>
2015-01-18 18:44:31 +0100

    Modified python for opencv2

---
+++ wrappers/python/demo_cv_sync2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import freenect
+import cv2
+import frame_convert
+
+print('Press ESC in window to stop')
+
+
+def get_depth():
+    return frame_convert.pretty_depth_cv(freenect.sync_get_depth()[0])
+
+
+def get_video():
+    return frame_convert.video_cv(freenect.sync_get_video()[0])
+
+
+while 1:
+    cv2.imshow('Depth', get_depth())
+    cv2.imshow('Video', get_video())
+    if cv2.WaitKey(10) == 27:
+        break
